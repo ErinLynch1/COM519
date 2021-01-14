382573a2772d2691a3aa0b3aaf9083d959f634af
Erin Lynch <erinlynch@Erins-MacBook-Pro.local>
2021-01-14 12:58:08 +0000

    latest

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const chalk = require("chalk");
 const bodyParser = require("body-parser");
 const expressSession = require("express-session");
-const User = require("./models/User");
+const User = require("./models/user");
 
 
 /**
